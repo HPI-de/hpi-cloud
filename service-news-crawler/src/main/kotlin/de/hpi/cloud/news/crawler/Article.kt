c6f12e7596e9f97b789440fd88a292c4aab19645
Jonas Wanke <contact@jonas-wanke.com>
2019-08-25 16:51:25 +0200

    feat(common, service-news-crawler): simplify json-ing in entities

--- service-news-crawler/src/main/kotlin/de/hpi/cloud/news/crawler/Article.kt
+++ service-news-crawler/src/main/kotlin/de/hpi/cloud/news/crawler/Article.kt
@@ -1,10 +1,8 @@
 package de.hpi.cloud.news.crawler
 
-import com.couchbase.client.java.document.json.JsonObject
 import de.hpi.cloud.common.Entity
 import de.hpi.cloud.common.utils.couchbase.i18nSingle
 import de.hpi.cloud.common.utils.protobuf.ImageSize
-import de.hpi.cloud.common.utils.protobuf.timestampNow
 import de.hpi.cloud.common.utils.protobuf.toDbMap
 import de.hpi.cloud.common.utils.protobuf.toTimestamp
 import java.net.URL
@@ -30,21 +28,13 @@ data class Article(
         )
     }
 
-    override fun toJsonObject(): JsonObject = JsonObject.from(
-        mapOf(
-            "type" to type,
-            "version" to version,
-            "id" to id,
-            "fetchedAt" to timestampNow(),
-            "value" to mapOf(
-                "title" to i18nSingle(preview.title, preview.language),
-                "link" to i18nSingle(preview.url.toString(), preview.language),
-                "sourceId" to preview.source.id,
-                "publishedAt" to preview.publishedAt.toTimestamp().toDbMap(),
-                "cover" to cover?.asMap(this),
-                "teaser" to i18nSingle(preview.teaser, preview.language),
-                "content" to i18nSingle(content, preview.language)
-            )
-        )
+    override fun valueToMap() = mapOf(
+        "title" to i18nSingle(preview.title, preview.language),
+        "link" to i18nSingle(preview.url.toString(), preview.language),
+        "sourceId" to preview.source.id,
+        "publishedAt" to preview.publishedAt.toTimestamp().toDbMap(),
+        "cover" to cover?.asMap(this),
+        "teaser" to i18nSingle(preview.teaser, preview.language),
+        "content" to i18nSingle(content, preview.language)
     )
 }
