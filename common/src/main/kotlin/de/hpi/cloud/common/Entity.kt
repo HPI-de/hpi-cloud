97fa125a4604f9d8ea74ed4eb10ba902304794dc
Jonas Wanke <contact@jonas-wanke.com>
2019-08-28 14:39:49 +0200

    refactor(service-common, common): move JSON doc builder to common

--- common/src/main/kotlin/de/hpi/cloud/common/Entity.kt
+++ common/src/main/kotlin/de/hpi/cloud/common/Entity.kt
@@ -1,13 +1,9 @@
 package de.hpi.cloud.common
 
-import com.couchbase.client.java.document.JsonDocument
-import com.couchbase.client.java.document.json.JsonObject
-import de.hpi.cloud.common.utils.couchbase.*
+import de.hpi.cloud.common.utils.couchbase.buildJsonDocument
 import de.hpi.cloud.common.utils.protobuf.timestampNow
 
 abstract class Entity(val type: String, val version: Int) {
-    val documentId
-        get() = "$type:$id"
     abstract val id: String
     open val fetchedAt = timestampNow()
 
@@ -15,17 +11,6 @@ abstract class Entity(val type: String, val version: Int) {
     abstract fun valueToMap(): Map<String, Any?>
 
     open fun metaToMap(): Map<String, Any?> = mapOf("fetchedAt" to fetchedAt)
-    fun toJsonObject(): JsonObject =
-        JsonObject.from(
-            mapOf(
-                KEY_TYPE to type,
-                KEY_VERSION to version,
-                KEY_ID to id,
-                KEY_METADATA to metaToMap(),
-                KEY_VALUE to valueToMap()
-            )
-        )
-
-    fun toJsonDocument(): JsonDocument = JsonDocument.create(documentId, toJsonObject())
+    fun toJsonDocument() = buildJsonDocument(id, type, version, valueToMap(), metaToMap())
     // endregion
 }
--- service-common/src/main/kotlin/de/hpi/cloud/common/utils/grpc/Builder.kt
+++ service-common/src/main/kotlin/de/hpi/cloud/common/utils/grpc/Builder.kt
@@ -1,24 +1,8 @@
 package de.hpi.cloud.common.utils.grpc
 
-import com.couchbase.client.java.document.JsonDocument
 import com.couchbase.client.java.document.json.JsonObject
 import com.google.protobuf.GeneratedMessageV3.Builder
-import de.hpi.cloud.common.utils.couchbase.KEY_ID
-import de.hpi.cloud.common.utils.couchbase.KEY_TYPE
 import de.hpi.cloud.common.utils.couchbase.KEY_VALUE
-import de.hpi.cloud.common.utils.couchbase.KEY_VERSION
-
-fun buildJsonDocument(id: String, type: String, version: Int, value: Map<String, Any?>): JsonDocument {
-    return JsonDocument.create(
-        "$type:$id",
-        JsonObject.from(mapOf(
-            KEY_TYPE to type,
-            KEY_VERSION to version,
-            KEY_ID to id,
-            KEY_VALUE to value
-        ))
-    )
-}
 
 fun <M : Any, B : Builder<B>> B.buildWith(json: JsonObject?, builder: B.(JsonObject) -> Unit): M? {
     val value = json?.getObject(KEY_VALUE) ?: return null
