942ecba39c59b37bc330f0eccff0ffaf49ca9c2f
Maximilian Stiede <maximilian.stiede@student.hpi.uni-potsdam.de>
2019-10-06 17:03:33 +0200

    feat(service-course-crawler): add database upserting

--- service-course-crawler/src/main/kotlin/de/hpi/cloud/course/crawler/Main.kt
+++ service-course-crawler/src/main/kotlin/de/hpi/cloud/course/crawler/Main.kt
@@ -1,12 +1,14 @@
 package de.hpi.cloud.course.crawler
 
 import de.hpi.cloud.common.Entity
+import de.hpi.cloud.common.utils.couchbase.withBucket
 import org.jsoup.Connection
 import org.jsoup.Jsoup
 import java.io.File
 import java.io.PrintWriter
 import java.net.URI
 import java.net.URL
+import java.util.concurrent.atomic.AtomicInteger
 
 const val NAME = "HPI-MobileDev-Crawler[Courses]"
 val USER_AGENT_TEMPLATE = "$NAME%s jsoup/1.12.1 Kotlin-runtime/${KotlinVersion.CURRENT}"
@@ -23,7 +25,9 @@ val CRAWLERS = setOf(
     HpiCourseListCrawler("Cybersecurity", Degree.MASTER)
 )
 
+val requestCount = AtomicInteger(0)
 fun createDocumentQuery(url: URL): Connection {
+    requestCount.incrementAndGet()
     return Jsoup
         .connect(url.toString())
         .userAgent(USER_AGENT_STRING)
@@ -32,50 +36,28 @@ fun createDocumentQuery(url: URL): Connection {
 fun main() {
     println("Starting $NAME")
     println("Crawling current semester") // TODO: implement archive crawler
+    println("Using User-Agent=\"$USER_AGENT_STRING\"")
 
-    CRAWLERS
-        .stream()
-        .flatMap {
-            println("Starting crawler for ${it}")
-            println("Using User-Agent=\"$USER_AGENT_STRING\"")
-            it.listCourses()
-        }
-        .map { it.query() }
-        .forEach { cd ->
-            val name = cd.id
-            File("./output/$name.txt")
-                .also { it.createNewFile() }
-                .printWriter()
-                .use { pw ->
-                    println("writing $name")
-                    pw.print("CourseDetail[${cd.id}]")
-                    pw.printlnEntity(cd)
-                    pw.print("Course[${cd.course.id}]")
-                    pw.printlnEntity(cd.course)
-                    pw.print("CourseSeries[${cd.course.courseSeries.id}]")
-                    pw.printlnEntity(cd.course.courseSeries)
-                }
-        }
+    withBucket("course") { bucket ->
+        var count = 0
+        CRAWLERS
+            .stream()
+            .flatMap {
+                println("Starting crawler for ${it}")
+                it.listCourses()
+            }
+            .map { it.query() }
+            .parallel()
+            .forEach {
+                val name = it.id
 
-//        withBucket("news") { bucket ->
-//            var count = 0
-//            articles.forEach {
-//                println("Parsed article with ID ${it.id}")
-//                bucket.upsert(it.toJsonDocument())
-//                count++
-//            }
-//            println("Upserted $count articles")
-//        }
-//        println("Crawler used ${crawler.metrics.requestCount} server requests")
-}
-
-fun PrintWriter.printlnEntity(entity: Entity) {
-    printlnMap(entity.valueToMap())
-}
-fun PrintWriter.printlnMap(map: Map<String, Any?>) {
-    println(map.entries.joinToString(
-        separator = ",\n",
-        prefix = "{\n",
-        postfix = "\n}"
-    ) { "\"${it.key}\": ${it.value}" })
+                println("Parsed course page with ID ${it.id}")
+                bucket.upsert(it.toJsonDocument())
+                bucket.upsert(it.course.toJsonDocument())
+                bucket.upsert(it.course.courseSeries.toJsonDocument())
+                count++
+            }
+        println("Upserted $count course page")
+    }
+    println("Crawler used ${requestCount} server requests")
 }
