83bd4d24d66b57a7283bf3aac22e80b56358a0e3
Jonas Wanke <contact@jonas-wanke.com>
2019-12-18 22:05:29 +0100

    feat(news): use new common, API

--- service-news/src/main/kotlin/de/hpi/cloud/news/NewsService.kt
+++ service-news/src/main/kotlin/de/hpi/cloud/news/NewsService.kt
@@ -1,18 +1,27 @@
 package de.hpi.cloud.news
 
 import com.couchbase.client.java.Bucket
-import com.couchbase.client.java.document.json.JsonObject
 import com.couchbase.client.java.view.ViewQuery
-import com.google.protobuf.GeneratedMessageV3
-import com.google.protobuf.UInt32Value
 import de.hpi.cloud.common.Service
-import de.hpi.cloud.common.utils.couchbase.*
-import de.hpi.cloud.common.utils.getI18nString
-import de.hpi.cloud.common.utils.grpc.*
-import de.hpi.cloud.common.utils.protobuf.getImage
-import de.hpi.cloud.common.utils.protobuf.getTimestamp
+import de.hpi.cloud.common.couchbase.VIEW_BY_ID
+import de.hpi.cloud.common.couchbase.get
+import de.hpi.cloud.common.couchbase.paginate
+import de.hpi.cloud.common.entity.Id
+import de.hpi.cloud.common.grpc.checkArgRequired
+import de.hpi.cloud.common.grpc.notFound
+import de.hpi.cloud.common.grpc.unary
+import de.hpi.cloud.common.protobuf.build
+import de.hpi.cloud.news.entities.*
+import de.hpi.cloud.news.entities.Article
+import de.hpi.cloud.news.entities.Category
+import de.hpi.cloud.news.entities.Source
+import de.hpi.cloud.news.entities.Tag
 import de.hpi.cloud.news.v1test.*
 import io.grpc.stub.StreamObserver
+import de.hpi.cloud.news.v1test.Article as ProtoArticle
+import de.hpi.cloud.news.v1test.Category as ProtoCategory
+import de.hpi.cloud.news.v1test.Source as ProtoSource
+import de.hpi.cloud.news.v1test.Tag as ProtoTag
 
 fun main(args: Array<String>) {
     val service = Service("news", args.firstOrNull()?.toInt()) { NewsServiceImpl(it) }
@@ -22,7 +31,6 @@ fun main(args: Array<String>) {
 class NewsServiceImpl(private val bucket: Bucket) : NewsServiceGrpc.NewsServiceImplBase() {
     companion object {
         const val DESIGN_ARTICLE = "article"
-        const val DESIGN_SOURCE = "source"
         const val DESIGN_CATEGORY = "category"
         const val DESIGN_TAG = "tag"
     }
@@ -31,67 +39,43 @@ class NewsServiceImpl(private val bucket: Bucket) : NewsServiceGrpc.NewsServiceI
     override fun listArticles(
         request: ListArticlesRequest?,
         responseObserver: StreamObserver<ListArticlesResponse>?
-    ) = unary(request, responseObserver, "listArticles") { req ->
+    ) = unary(request, responseObserver, "listArticles") { context, req ->
         val (articles, newToken) = ViewQuery.from(DESIGN_ARTICLE, VIEW_BY_ID)
-            .paginate(bucket, req.pageSize, req.pageToken) { it.parseArticle(req) }
+            .paginate<Article>(bucket, req.pageSize, req.pageToken)
 
-        ListArticlesResponse.newBuilder().buildWith {
-            addAllArticles(articles)
+        ListArticlesResponse.newBuilder().build {
+            addAllArticles(articles.map { it.toProto(context) })
             nextPageToken = newToken
         }
     }
 
-    override fun getArticle(request: GetArticleRequest?, responseObserver: StreamObserver<Article>?) =
-        unary(request, responseObserver, "getArticle") { req ->
+    override fun getArticle(request: GetArticleRequest?, responseObserver: StreamObserver<ProtoArticle>?) =
+        unary(request, responseObserver, "getArticle") { context, req ->
             checkArgRequired(req.id, "id")
 
-            bucket.getContent(DESIGN_ARTICLE, VIEW_BY_ID, req.id)?.parseArticle(req)
-                ?: notFound<Article>(req.id)
-        }
-
-    private fun JsonObject.parseArticle(request: GeneratedMessageV3) =
-        Article.newBuilder().buildWithDocument<Article, Article.Builder>(this) {
-            id = getString(KEY_ID)
-            sourceId = it.getString("sourceId")
-            link = it.getI18nString("link", request)
-            title = it.getI18nString("title", request)
-            publishDate = it.getTimestamp("publishedAt")
-            addAllAuthorIds(it.getStringArray("authorIds").filterNotNull())
-            it.getImage("cover", request)?.let { i -> cover = i }
-            teaser = it.getI18nString("teaser", request)
-            content = it.getI18nString("content", request)
-            addAllCategories(it.getStringArray("categories").filterNotNull()
-                .mapNotNull { c -> getCategory(c, request) })
-            addAllTags(it.getStringArray("tags").filterNotNull().mapNotNull { t -> getTag(t, request) })
-            it.getInt("viewCount")?.let { c -> viewCount = UInt32Value.of(c) }
+            bucket.get<Article>(Id(req.id))?.toProto(context)
+                ?: notFound<ProtoArticle>(req.id)
         }
     // endregion
 
     // region Source
     override fun listSources(request: ListSourcesRequest?, responseObserver: StreamObserver<ListSourcesResponse>?) =
-        unary(request, responseObserver, "listSources") { req ->
-            val (sources, newToken) = ViewQuery.from(DESIGN_SOURCE, VIEW_BY_ID)
-                .paginate(bucket, req.pageSize, req.pageToken) { it.parseSource(req) }
+        unary(request, responseObserver, "listSources") { context, req ->
+            val (sources, newToken) = ViewQuery.from(Source.type, VIEW_BY_ID)
+                .paginate<Source>(bucket, req.pageSize, req.pageToken)
 
-            ListSourcesResponse.newBuilder().buildWith {
-                addAllSources(sources)
+            ListSourcesResponse.newBuilder().build {
+                addAllSources(sources.map { it.toProto(context) })
                 nextPageToken = newToken
             }
         }
 
-    override fun getSource(request: GetSourceRequest?, responseObserver: StreamObserver<Source>?) =
-        unary(request, responseObserver, "getSource") { req ->
+    override fun getSource(request: GetSourceRequest?, responseObserver: StreamObserver<ProtoSource>?) =
+        unary(request, responseObserver, "getSource") { context, req ->
             checkArgRequired(req.id, "id")
 
-            bucket.getContent(DESIGN_SOURCE, VIEW_BY_ID, req.id)?.parseSource(req)
-                ?: notFound<Source>(req.id)
-        }
-
-    private fun JsonObject.parseSource(request: GeneratedMessageV3) =
-        Source.newBuilder().buildWithDocument<Source, Source.Builder>(this) {
-            id = getString(KEY_ID)
-            title = it.getI18nString("title", request)
-            link = it.getI18nString("link", request)
+            bucket.get<Source>(Id(req.id))?.toProto(context)
+                ?: notFound<ProtoSource>(req.id)
         }
     // endregion
 
@@ -99,64 +83,43 @@ class NewsServiceImpl(private val bucket: Bucket) : NewsServiceGrpc.NewsServiceI
     override fun listCategories(
         request: ListCategoriesRequest?,
         responseObserver: StreamObserver<ListCategoriesResponse>?
-    ) = unary(request, responseObserver, "listCategories") { req ->
+    ) = unary(request, responseObserver, "listCategories") { context, req ->
         val (categories, newToken) = ViewQuery.from(DESIGN_CATEGORY, VIEW_BY_ID)
-            .paginate(bucket, req.pageSize, req.pageToken) { it.parseCategory(req) }
+            .paginate<Category>(bucket, req.pageSize, req.pageToken)
 
-        ListCategoriesResponse.newBuilder().buildWith {
-            addAllCategories(categories)
+        ListCategoriesResponse.newBuilder().build {
+            addAllCategories(categories.map { it.toProto(context) })
             nextPageToken = newToken
         }
     }
 
-    override fun getCategory(request: GetCategoryRequest?, responseObserver: StreamObserver<Category>?) =
-        unary(request, responseObserver, "getCategory") { req ->
+    override fun getCategory(request: GetCategoryRequest?, responseObserver: StreamObserver<ProtoCategory>?) =
+        unary(request, responseObserver, "getCategory") { context, req ->
             checkArgRequired(req.id, "id")
 
-            getCategory(req.id, req)
-                ?: notFound<Category>(req.id)
-        }
-
-    private fun getCategory(id: String, request: GeneratedMessageV3): Category? {
-        return bucket.getContent(DESIGN_CATEGORY, VIEW_BY_ID, id)?.parseCategory(request)
-    }
-
-    private fun JsonObject.parseCategory(request: GeneratedMessageV3) =
-        Category.newBuilder().buildWithDocument<Category, Category.Builder>(this) {
-            id = getString(KEY_ID)
-            title = it.getI18nString("title", request)
+            bucket.get<Category>(Id(req.id))?.toProto(context)
+                ?: notFound<ProtoCategory>(req.id)
         }
     // endregion
 
     // region Tag
     override fun listTags(request: ListTagsRequest?, responseObserver: StreamObserver<ListTagsResponse>?) =
-        unary(request, responseObserver, "listTags") { req ->
+        unary(request, responseObserver, "listTags") { context, req ->
             val (tags, newToken) = ViewQuery.from(DESIGN_TAG, VIEW_BY_ID)
-                .paginate(bucket, req.pageSize, req.pageToken) { it.parseTag(req) }
+                .paginate<Tag>(bucket, req.pageSize, req.pageToken)
 
-            ListTagsResponse.newBuilder().buildWith {
-                addAllTags(tags)
+            ListTagsResponse.newBuilder().build {
+                addAllTags(tags.map { it.toProto(context) })
                 nextPageToken = newToken
             }
         }
 
-    override fun getTag(request: GetTagRequest?, responseObserver: StreamObserver<Tag>?) =
-        unary(request, responseObserver, "getTag") { req ->
+    override fun getTag(request: GetTagRequest?, responseObserver: StreamObserver<ProtoTag>?) =
+        unary(request, responseObserver, "getTag") { context, req ->
             checkArgRequired(req.id, "id")
 
-            getTag(req.id, req)
-                ?: notFound<Tag>(req.id)
-        }
-
-    private fun getTag(id: String, request: GeneratedMessageV3): Tag? {
-        return bucket.getContent(DESIGN_TAG, VIEW_BY_ID, id)?.parseTag(request)
-    }
-
-    private fun JsonObject.parseTag(request: GeneratedMessageV3) =
-        Tag.newBuilder().buildWithDocument<Tag, Tag.Builder>(this) {
-            id = getString(KEY_ID)
-            title = it.getI18nString("title", request)
-            articleCount = it.getInt("articleCount") ?: 0
+            bucket.get<Tag>(Id(req.id))?.toProto(context)
+                ?: notFound<ProtoTag>(req.id)
         }
     // endregion
 }
---
+++ service-news/src/main/kotlin/de/hpi/cloud/news/entities/Source.kt
@@ -0,0 +1,36 @@
+package de.hpi.cloud.news.entities
+
+import de.hpi.cloud.common.Context
+import de.hpi.cloud.common.entity.Entity
+import de.hpi.cloud.common.entity.Wrapper
+import de.hpi.cloud.common.protobuf.builder
+import de.hpi.cloud.common.types.L10n
+import de.hpi.cloud.common.types.l10n
+import de.hpi.cloud.common.utils.parseUri
+import kotlinx.serialization.ContextualSerialization
+import kotlinx.serialization.Serializable
+import java.net.URI
+import de.hpi.cloud.news.v1test.Source as ProtoSource
+
+@Serializable
+data class Source(
+    val title: L10n<String>,
+    val link: L10n<@ContextualSerialization URI>
+) : Entity<Source>() {
+    companion object : Entity.Companion<Source>("source")
+
+    object ProtoSerializer : Entity.ProtoSerializer<Source, ProtoSource, ProtoSource.Builder>() {
+        override fun fromProto(proto: ProtoSource, context: Context): Source = Source(
+            title = proto.title.l10n(context),
+            link = proto.link.parseUri().l10n(context)
+        )
+
+        override fun toProtoBuilder(entity: Source, context: Context): ProtoSource.Builder =
+            ProtoSource.newBuilder().builder(entity) {
+                title = it.title[context]
+                link = it.link[context].toString()
+            }
+    }
+}
+
+fun Wrapper<Source>.toProto(context: Context): ProtoSource = Source.ProtoSerializer.toProto(this, context)
