706ca8f81d13b7a293a98d8fce7bbe86f9c4f2a6
Jonas Wanke <contact@jonas-wanke.com>
2020-01-05 12:20:54 +0100

    fix(common): remove unused String trim+validation functions

--- common/src/main/kotlin/de/hpi/cloud/common/utils/String.kt
+++ common/src/main/kotlin/de/hpi/cloud/common/utils/String.kt
@@ -1,19 +1,5 @@
 package de.hpi.cloud.common.utils
 
-import de.hpi.cloud.common.types.L10n
-
-fun String.trimNotEmpty(): String = trim().also {
-    require(it.isNotEmpty()) { "String must not be blank" }
-}
-
-fun L10n<String>.trimNotEmpty(): L10n<String> =
-    L10n(
-        values.mapValues { (locale, value) ->
-            value.trim().also {
-                require(it.isNotEmpty()) { "String must not be blank (locale $locale" }
-            }
-        }
-    )
 
 fun String.cut(vararg delimiters: Char, ignoreCase: Boolean = false) =
     indexOfAny(delimiters, ignoreCase = ignoreCase).let { index ->
